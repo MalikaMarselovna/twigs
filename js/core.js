7cce96134b9178bd3056cbe4835b6297ab995426
MalikaMarselovna <mmukashova1@gmail.com>
2024-02-11 19:23:10 +0600

    start creating 7-section

--- js/core.js
+++ js/core.js
@@ -1,15 +1,32 @@
-let slider = document.getElementById('slider');
-let cards = slider.querySelectorAll('.s_card');
-let currentIndex = 0;
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+for (let smoothLink of smoothLinks) {
+    smoothLink.addEventListener('click', function (e) {
+        e.preventDefault();
+        const id = smoothLink.getAttribute('href');
 
-function scrollSlider(direction) {
-    currentIndex += direction;
-    if (currentIndex < 0) {
-        currentIndex = cards.length - 1;
-    } else if (currentIndex >= cards.length) {
-        currentIndex = 0;
-    }
-    cards[currentIndex].scrollIntoView({ behavior: 'smooth', block: 'center' });
-}
+        document.querySelector(id).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+};
 
+setTimeout(function (){
+    loader.remove();
+    section.style.display = 'flex';
+    header.style.display = 'flex';
+    footer.style.display = ' ';
+    order.style.display = 'flex';
+    about.style.display = 'flex';
+    start.style.display = 'flex';
+    advantages.style.display = 'flex';
+    partners.style.display = 'flex';
+    program.style.display = 'flex';
+    mentors.style.display = 'flex';
+    form.style.display = 'flex';
+}, 2000)
+
+
+
+console.log($('.btn'))
 
