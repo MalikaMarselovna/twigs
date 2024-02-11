fc0353d870eb7f79c7e73a3f92c616182a15e898
MalikaMarselovna <mmukashova1@gmail.com>
2024-02-11 19:18:50 +0600

    finished 4-section

--- js/core.js
+++ js/core.js
@@ -1,45 +1,15 @@
-const track = document.getElementById("image-track");
+let slider = document.getElementById('slider');
+let cards = slider.querySelectorAll('.s_card');
+let currentIndex = 0;
 
-const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
-
-const handleOnUp = () => {
-  track.dataset.mouseDownAt = "0";  
-  track.dataset.prevPercentage = track.dataset.percentage;
+function scrollSlider(direction) {
+    currentIndex += direction;
+    if (currentIndex < 0) {
+        currentIndex = cards.length - 1;
+    } else if (currentIndex >= cards.length) {
+        currentIndex = 0;
+    }
+    cards[currentIndex].scrollIntoView({ behavior: 'smooth', block: 'center' });
 }
 
-const handleOnMove = e => {
-  if(track.dataset.mouseDownAt === "0") return;
-  
-  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
-        maxDelta = window.innerWidth / 2;
-  
-  const percentage = (mouseDelta / maxDelta) * -100,
-        nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
-        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
-  
-  track.dataset.percentage = nextPercentage;
-  
-  track.animate({
-    transform: `translate(${nextPercentage}%, -50%)`
-  }, { duration: 1200, fill: "forwards" });
-  
-  for(const image of track.getElementsByClassName("image")) {
-    image.animate({
-      objectPosition: `${100 + nextPercentage}% center`
-    }, { duration: 1200, fill: "forwards" });
-  }
-}
 
-/* -- Had to add extra lines for touch events -- */
-
-window.onmousedown = e => handleOnDown(e);
-
-window.ontouchstart = e => handleOnDown(e.touches[0]);
-
-window.onmouseup = e => handleOnUp(e);
-
-window.ontouchend = e => handleOnUp(e.touches[0]);
-
-window.onmousemove = e => handleOnMove(e);
-
-window.ontouchmove = e => handleOnMove(e.touches[0]);
